a1b24774d29b24fa226106ed9805b06e242f84e1
Are <alextorres4101@gmail.com>
2022-01-28 16:22:45 +0100

    added full_packet to interface

--- examples/Ts26_Bot/Module.h
+++ examples/Ts26_Bot/Module.h
@@ -5,7 +5,7 @@
 class Module
 {
 public:
-    virtual void on_send(const std::vector<std::string>& packet_splitted) {}
-    virtual void on_recv(const std::vector<std::string>& packet_splitted) {}
+    virtual void on_send(const std::vector<std::string>& packet_splitted, const std::string& full_packet) {}
+    virtual void on_recv(const std::vector<std::string>& packet_splitted, const std::string& full_packet) {}
 };
 
