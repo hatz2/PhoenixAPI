3cf3052a456760a86a413b7a16d4ff05c5b6bba3
Hatz <alextorres4101@gmail.com>
2022-07-13 13:32:26 +0200

    Updated tabs

--- examples/InstantCombat/Logger.cpp
+++ examples/InstantCombat/Logger.cpp
@@ -2,17 +2,17 @@
 
 namespace
 {
-	std::mutex mtx;
+    std::mutex mtx;
 }
 
 void Logger::print(const std::string& text)
 {
-	std::lock_guard<std::mutex> lock(mtx);
-	std::cout << text;
+    std::lock_guard<std::mutex> lock(mtx);
+    std::cout << text;
 }
 
 void Logger::error(const std::string& text)
 {
-	std::lock_guard<std::mutex> lock(mtx);
-	std::cerr << text;
+    std::lock_guard<std::mutex> lock(mtx);
+    std::cerr << text;
 }
