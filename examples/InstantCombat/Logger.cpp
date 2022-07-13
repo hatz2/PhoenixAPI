c7c8df6b53a05275a98b76d713622b8b52375094
Hatz <alextorres4101@gmail.com>
2022-07-13 13:23:17 +0200

    Added safe thread console logger

---
+++ examples/InstantCombat/Logger.cpp
@@ -0,0 +1,18 @@
+#include "Logger.h"
+
+namespace
+{
+	std::mutex mtx;
+}
+
+void Logger::print(const std::string& text)
+{
+	std::lock_guard<std::mutex> lock(mtx);
+	std::cout << text;
+}
+
+void Logger::error(const std::string& text)
+{
+	std::lock_guard<std::mutex> lock(mtx);
+	std::cerr << text;
+}
---
+++ examples/InstantCombat/Logger.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <iostream>
+#include <string>
+#include <mutex>
+
+namespace Logger
+{
+	void print(const std::string& text);
+	void error(const std::string& text);
+}
